e54ec2a338cad0ec5e9caf30a92f075df582e889
YadhiPrograma <yadhiprogramacion@gmail.com>
2023-08-08 12:17:21 -0300

    cambio
    hola

---
+++ test/NewMain.java
@@ -0,0 +1,30 @@
+
+import java.awt.BorderLayout;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yadhi
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        System.out.println("HOLAAAAA");
+        
+        System.out.println("ahhgdhgbfdngfxgdfahgchgcgshshs"
+                + "");
+        
+    }
+    
+}
